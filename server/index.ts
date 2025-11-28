922b2add9acaadd7358c71cf6d699a1a111f2924
Your Name <your.email@example.com>
2025-11-28 14:06:56 +0530

    index

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,6 @@ import express, { type Request, Response, NextFunction } from "express";
 import cors from "cors";
 // @ts-ignore
 import { registerRoutes } from "./routes.js";
-import dotenv from "dotenv";
-
-// Load environment variables from .env file
-dotenv.config();
 
 function log(message: string, source = "express") {
   const formattedTime = new Date().toLocaleTimeString("en-US", {
